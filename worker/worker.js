270ad3fd37a2b552da7f9a5c1953ffdffb91c0f9
huangchinlin <huangchin.lin@gmail.com>
2021-10-22 16:37:42 +0800

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -3,12 +3,12 @@ onmessage = (e) => {
   switch(e.data.topic) {
     case "1":
       for(let i = 0; i < 100000; i++);
-      ret = e.data.result = "ran 1,000,000 times";
+      ret = e.data.result = "case 1: ran 100,000 times";
       console.log(ret);
       break;
     case "2":
       for(let i=0; i < 500000; i++);
-      ret = e.data.result = "ran 1,000,000,000 times";
+      ret = e.data.result = "case 2: ran 500,000 times";
       console.log(ret);
       break;
     default:
